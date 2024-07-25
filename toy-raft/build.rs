5ae27f0780b8c659a88b3393d8cb0a2d95680360
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2024-07-24 19:21:02 -0700

    feat: use Bazel

--- toy-raft/build.rs
+++ toy-raft/build.rs
@@ -1,6 +1,22 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
+    #[cfg(feature = "bazel")]
+    {
+        let r = runfiles::Runfiles::create()?;
+        let protoc_loc = std::env::var("PROTOC_RLOCATION").expect("PROTOC_RLOCATION must be set");
+        let protoc_path = r.rlocation(protoc_loc.as_str());
+        let protoc_dir = protoc_path.parent().unwrap();
+        std::env::set_var(
+            "PATH",
+            format!(
+                "{}:{}",
+                protoc_dir.to_str().unwrap(),
+                std::env::var("PATH").unwrap(),
+            ),
+        );
+    }
+
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
