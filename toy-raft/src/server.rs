8e3a29d154b499a6f5aa03f243eee62809990971
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2024-07-28 06:55:09 -0700

    feat: add minimal access logging

--- toy-raft/src/server.rs
+++ toy-raft/src/server.rs
@@ -3,6 +3,10 @@ use std::net::ToSocketAddrs;
 use std::sync::Arc;
 use std::sync::Mutex;
 
+use tower::layer;
+use tower::layer::layer_fn;
+use tracing::{error, info};
+
 use crate::config;
 use crate::grpc;
 use crate::raft;
@@ -12,6 +16,73 @@ pub struct Server {
     addr: SocketAddr,
     state: Arc<Mutex<raft::Raft>>,
     log: Arc<Mutex<grpc::LogEntry>>,
+}
+
+#[derive(Debug, Clone)]
+struct AccessLogging<S> {
+    inner: S,
+}
+
+// Alternative: https://github.com/hyperium/tonic/blob/2d9791198fdec1b2e8530bd3365d0c0ddb290f4c/examples/src/tower/server.rs
+
+impl<S, Request> tower::Service<Request> for AccessLogging<S>
+where
+    S: tower::Service<Request> + Clone + Send + 'static,
+    S::Error: std::fmt::Display,
+    S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = AccessLoggingFuture<S::Future>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+    fn call(&mut self, request: Request) -> Self::Future {
+        let f = self.inner.call(request);
+        AccessLoggingFuture {
+            inner_future: f,
+            start: std::time::Instant::now(),
+        }
+    }
+}
+
+pin_project_lite::pin_project! {
+    struct AccessLoggingFuture<F> {
+        #[pin]
+        inner_future: F,
+        start: std::time::Instant,
+    }
+}
+
+impl<F, Response, Error> std::future::Future for AccessLoggingFuture<F>
+where
+    F: std::future::Future<Output = Result<Response, Error>>,
+    Error: std::fmt::Display,
+{
+    type Output = F::Output;
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let project = self.project();
+
+        match project.inner_future.poll(cx) {
+            std::task::Poll::Ready(result) => {
+                match &result {
+                    Ok(_) => info!(elapsed = project.start.elapsed().as_secs_f64(), "Access"),
+                    Err(e) => error!(err = e.to_string(), "Access"),
+                }
+
+                std::task::Poll::Ready(result)
+            }
+            std::task::Poll::Pending => std::task::Poll::Pending,
+        }
+    }
 }
 
 impl Server {
@@ -45,6 +116,7 @@ impl Server {
         let server = Arc::new(self);
 
         match tonic::transport::Server::builder()
+            .layer(layer_fn(|service| AccessLogging { inner: service }))
             .add_service(grpc::raft_server::RaftServer::new(server.clone()))
             .add_service(grpc::operations_server::OperationsServer::new(
                 server.clone(),
