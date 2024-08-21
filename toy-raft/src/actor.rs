7175c6ce0a67dc59de290ba20845a7c908ba5537
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2024-08-21 14:47:53 -0700

    docs: leave a TODO comment to describe reconnection issue

--- toy-raft/src/actor.rs
+++ toy-raft/src/actor.rs
@@ -35,6 +35,11 @@ impl Actor {
             .map(|peer| {
                 // Config has already been validated.
                 let url = peer.addr.parse::<http::Uri>().unwrap();
+
+                // TODO: the current client doesn't reconnect automatically.
+                // Support reconnection.
+                //
+                // See: https://github.com/hyperium/tonic/issues/1254
                 PeerClient {
                     id: Arc::new(peer.id.clone()),
                     client: grpc::raft_client::RaftClient::new(
